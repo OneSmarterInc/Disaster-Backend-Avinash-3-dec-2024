4bb3d5b44b5276fd768d4edf4e384bf277a0ab8a
kunal kalpande <kalpandelunal007@gmail.com>
2023-09-23 15:01:41 +0530

    intro added

--- disastermanagement/src/Components/MainRoutes.jsx
+++ disastermanagement/src/Components/MainRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from '../Pages/Home'
+import Intro from '../Pages/Intro'
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Home/>}  />
+        <Route path='/intro' element={<Intro />}/>
     </Routes>
   )
 }
